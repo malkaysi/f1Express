9b55aff1f66acf57a6093fc8203270db3dc41870
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2023-01-05 11:50:10 -0500

    Team creation

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -2,6 +2,7 @@ const Team = require("../models/team.js");
 const Car = require("../models/car");
 
 const async = require("async");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all Teams.
 exports.team_list = function (req, res, next) {
@@ -54,13 +55,58 @@ exports.team_detail = (req, res, next) => {
 
 // Display Team create form on GET.
 exports.team_create_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Team create GET");
+  res.render("team_form", { title: "Create a Team" })
 };
 
 // Handle Team create on POST.
-exports.team_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Team create POST");
-};
+exports.team_create_post = [
+  // Validate and sanitize fields
+  body("name")
+    .trim()
+    .isLength({ min: 1, max: 20 })
+    .escape()
+    .withMessage("Team name must be specified")
+    .isAlphanumeric()
+    .withMessage("Team name has non-alphanumeric characters."),
+  body("description")
+    .trim()
+    .isLength({ min: 1, max: 300 })
+    .escape()
+    .withMessage("Team description must be entered"),
+    
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract validation errors
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render("team_form", {
+        title: "Create Team",
+        team: req.body,
+        errors: errors.array(),
+      });
+      return;
+    }
+    // Data from form is valid.
+
+    // Create a team object 
+    const team = new Team({
+      name: req.body.name,
+      description: req.body.description
+    });
+
+    team.save((err) => {
+      if(err) {
+        return next(err)
+      }
+
+      // Successful, redirect new team url
+      res.redirect(team.url)
+    })
+  },
+];
 
 // Display Team delete form on GET.
 exports.team_delete_get = (req, res) => {
